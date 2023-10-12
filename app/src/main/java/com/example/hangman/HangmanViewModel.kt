9130a0be0d29090ab95159c2415f2f0e844e65ca
hrl-2024 <hrl@bu.edu>
2023-10-11 22:57:57 -0400

    Enable viewbinding + HangmanViewModel skeleton

---
+++ app/src/main/java/com/example/hangman/HangmanViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.hangman
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class HangmanViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+}
