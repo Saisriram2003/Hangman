d12e352dea93ec9aa0446042a8baef6b83e1f55c
Saisriram Gunturu <saisriram@saimail.com>
2023-10-13 17:14:25 -0400

    started backend

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.hangman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hangman.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var correctWord : String
+    private lateinit var currentGuessedWord : String
+    private var numTries = 0
+    private var maxTries = 6
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        generateNewGame()
     }
+
+    fun generateNewGame(){
+        correctWord = Words.gameWords[Random.nextInt(1, Words.gameWords.size)]
+        currentGuessedWord = "______"
+        binding.guessedWord.text = currentGuessedWord
+        numTries = 0
+        getCurrImageState(numTries)
+        updateGuessedWord(currentGuessedWord)
+    }
+
+
+    fun getCurrImageState(numTries: Int){
+        when(numTries){
+            0 -> binding.hangmanView.setImageResource(R.drawable.move0)
+            1 -> binding.hangmanView.setImageResource(R.drawable.move1)
+            2 -> binding.hangmanView.setImageResource(R.drawable.move2)
+            3 -> binding.hangmanView.setImageResource(R.drawable.move3)
+            4 -> binding.hangmanView.setImageResource(R.drawable.move4)
+            5 -> binding.hangmanView.setImageResource(R.drawable.move5)
+            6 -> binding.hangmanView.setImageResource(R.drawable.move6)
+        }
+    }
+
+    fun checkLetter(letter: Char){
+        var newGuessedWord = ""
+        for(i in correctWord.indices){
+            if(correctWord[i] == letter){
+                newGuessedWord += letter
+            }else{
+                newGuessedWord += currentGuessedWord[i]
+            }
+        }
+        currentGuessedWord = newGuessedWord
+        updateGuessedWord(currentGuessedWord)
+    }
+
+    fun updateGuessedWord(currentGuessedWord: String){
+        var guessedWordWithSpaces = ""
+        for(i in currentGuessedWord.indices){
+            guessedWordWithSpaces += currentGuessedWord[i] + " "
+        }
+        binding.guessedWord.text = guessedWordWithSpaces
+    }
+
 }
---
+++ app/src/main/java/com/example/hangman/Words.kt
@@ -0,0 +1,26 @@
+package com.example.hangman
+
+object Words {
+    val gameWords = listOf(
+        "Breeze",
+        "Castle",
+        "Fierce",
+        "Glisten",
+        "Hammer",
+        "Jungle",
+        "Kettle",
+        "Lizard",
+        "Mitten",
+        "Noodle",
+        "Outlet",
+        "Purple",
+        "Quiver",
+        "Rumble",
+        "Saddle",
+        "Tucker",
+        "Unicorn",
+        "Vessel",
+        "Waffle",
+        "Yellow"
+    )
+}
