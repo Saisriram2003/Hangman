f29cfa3d9b4c5b041dd4b1dd17cb8150030dd1a6
hrl-2024 <hrl@bu.edu>
2023-10-14 10:53:02 -0400

    Fixed Rotation Keyboard disappear bug

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.hangman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.hangman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val keyboardViewModel: KeyboardViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            // For rotating: without this, keyboard will disappear on rotation
+            supportFragmentManager.beginTransaction()
+                .add(R.id.keyboard, KeyboardFragment())
+                .commit()
+        }
     }
 }
