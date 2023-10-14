b63638f572e60298b5cbc2a307b0f5942bfd4950
Saisriram Gunturu <saisriram@saimail.com>
2023-10-14 18:29:42 -0400

    fixed game functionality

--- app/src/main/java/com/example/hangman/KeyboardViewModel.kt
+++ app/src/main/java/com/example/hangman/KeyboardViewModel.kt
@@ -5,10 +5,20 @@ import androidx.lifecycle.ViewModel
 
 
 const val KEYS = "Keys"
+const val TRIES = "Tries"
+const val CORRECT_WORD = "CorrectWord"
+const val GUESSED_WORD = "GuessedWord"
 
 class KeyboardViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     var keys : List<Key> get() = savedStateHandle.get(KEYS) ?: listOf()
         set(value) = savedStateHandle.set(KEYS, value)
+    var numTries : Int get() = savedStateHandle.get(TRIES) ?: 0
+        set(value) = savedStateHandle.set(TRIES, value)
+    var correctWord : String get() = savedStateHandle.get(CORRECT_WORD) ?: "hi"
+        set(value) = savedStateHandle.set(CORRECT_WORD, value)
+    var currentGuessedWord : String get() = savedStateHandle.get(GUESSED_WORD) ?: ""
+        set(value) = savedStateHandle.set(GUESSED_WORD, value)
+
 
 }
--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -9,15 +9,13 @@ import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hangman.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var correctWord : String
-    private lateinit var currentGuessedWord : String
-    private var numTries = 0
-    private var maxTries = 6
     private val keyboardViewModel: KeyboardViewModel by viewModels()
+    private val maxTries = 6
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +23,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (keyboardViewModel.keys.isEmpty()){
-            generateKeys()
+            keyboardViewModel.keys = generateKeys()
         }
         val adapter = KeyboardAdapter( keyboardViewModel.keys ){key ->
             if(key.isAvailable){
-                Toast.makeText(this, key.letter.toString(), Toast.LENGTH_SHORT).show()
+                play(key.letter)
                 key.isAvailable = false
             }
             else{
@@ -42,24 +40,65 @@ class MainActivity : AppCompatActivity() {
 
         generateNewGame()
     }
+    fun play(char: Char){
+        if(checkLetter(char)){
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show()
+            keyboardViewModel.numTries++
+            getCurrImageState(keyboardViewModel.numTries)
+        }
+        if(isGameFinished()){
+            if(keyboardViewModel.numTries == maxTries){
+                Toast.makeText(this, "You lost!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "You won!", Toast.LENGTH_SHORT).show()
+            }
+            generateNewGame()
+        }
+        Log.d("MainActivity","${keyboardViewModel.currentGuessedWord}")
 
-    fun generateKeys() {
+    }
+    fun isGameFinished(): Boolean{
+        if(keyboardViewModel.numTries == maxTries){
+            return true
+        }
+        else if(keyboardViewModel.currentGuessedWord.equals(keyboardViewModel.correctWord, true)){
+            return true
+        }
+        return false
+    }
+    fun generateKeys(): List<Key> {
         val keys = mutableListOf<Key>()
         for (i in 0 until 26) {
             val letter = 'A' + i
             keys += Key(letter, true)
         }
-        keyboardViewModel.keys = keys
+        return keys
     }
 
 
     fun generateNewGame(){
-        correctWord = Words.gameWords[Random.nextInt(1, Words.gameWords.size)]
-        currentGuessedWord = "______"
-        binding.guessedWord.text = currentGuessedWord
-        numTries = 0
-        getCurrImageState(numTries)
-        updateGuessedWord(currentGuessedWord)
+        keyboardViewModel.correctWord = Words.gameWords[Random.nextInt(1, Words.gameWords.size)]
+        keyboardViewModel.currentGuessedWord = "______"
+        binding.guessedWord.text = keyboardViewModel.currentGuessedWord
+        keyboardViewModel.numTries = 0
+        getCurrImageState(keyboardViewModel.numTries)
+        updateGuessedWord(keyboardViewModel.currentGuessedWord)
+        keyboardViewModel.keys = generateKeys()
+        val adapter = KeyboardAdapter( keyboardViewModel.keys ){key ->
+            if(key.isAvailable){
+                play(key.letter)
+                key.isAvailable = false
+            }
+            else{
+                Toast.makeText(this, "Already guessed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.keyboardRv?.adapter = adapter
+        Log.d("MainActivity", keyboardViewModel.correctWord)
     }
 
 
@@ -75,17 +114,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun checkLetter(letter: Char){
+    fun checkLetter(letter: Char): Boolean{
+        Log.d("MainActivity", "Letter: $letter")
+        Log.d("MainActivity", "Correct Word: ${keyboardViewModel.correctWord}")
         var newGuessedWord = ""
-        for(i in correctWord.indices){
-            if(correctWord[i] == letter){
+        var letterFound = false
+        for(i in keyboardViewModel.correctWord.indices){
+            Log.d("MainActivity", "Current Letter: ${keyboardViewModel.correctWord[i]}")
+            if(keyboardViewModel.correctWord[i].equals(letter,true)){
                 newGuessedWord += letter
+                letterFound = true
             }else{
-                newGuessedWord += currentGuessedWord[i]
+                newGuessedWord += keyboardViewModel.currentGuessedWord[i]
             }
         }
-        currentGuessedWord = newGuessedWord
-        updateGuessedWord(currentGuessedWord)
+        keyboardViewModel.currentGuessedWord = newGuessedWord
+        updateGuessedWord(keyboardViewModel.currentGuessedWord)
+        return letterFound
     }
 
     fun updateGuessedWord(currentGuessedWord: String){
