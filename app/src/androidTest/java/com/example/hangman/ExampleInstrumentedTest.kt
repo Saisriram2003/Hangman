617bcc1cff1af3290a2f2643f6135c4a32b550f6
Kristi Li <kli1226@bu.edu>
2023-10-16 19:23:30 -0400

    Added 2 Instrumented Tests

--- app/src/androidTest/java/com/example/hangman/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/hangman/ExampleInstrumentedTest.kt
@@ -1,24 +1,49 @@
 package com.example.hangman
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import android.widget.TextView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert.*
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.regex.Matcher
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+    @Before
+    fun setUp() {
+        scenario = launch(MainActivity::class.java)
+    }
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+//    Show recyclerview
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.hangman", appContext.packageName)
+    fun keyboardShown(){
+        onView(withId(R.id.keyboard_rv)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testRestartBtn(){
+        onView(withId(R.id.restart_button)).perform(click())
+        onView(withId(R.id.guessedWord)).check(matches(withText("_ _ _ _ _ _")));
     }
 }
